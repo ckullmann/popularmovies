4a476ab9da461de830305beb1706185adba3f19b
Christian Kullmann <christian.kullmann@gmail.com>
2016-07-03 22:58:31 +0200

    Initial commit

---
+++ app/src/main/java/de/christiankullmann/popularmovies/MovieOverviewFragment.java
@@ -0,0 +1,39 @@
+package de.christiankullmann.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MovieOverviewFragment extends Fragment {
+
+    public MovieOverviewFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        super.onCreateOptionsMenu(menu, inflater);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if(id == R.id.action_refresh) {
+
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
